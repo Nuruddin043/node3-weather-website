d3cdaa7f759414f22e8df02615d923b0a46d61fd
NURUDDIN <mdnuruddin043@gmail.com>
2020-07-12 20:20:57 +0600

    init commit

---
+++ src/app.js
@@ -0,0 +1,89 @@
+const path=require('path')
+const express=require('express')
+const hbs=require('hbs')
+const geoCode=require('./utils/geocode')
+const forecast=require('./utils/forecast')
+
+const app=express()
+
+const publicDirPath=path.join(__dirname, '../public')
+const viewsPath=path.join(__dirname,'../templates/views')
+const partialPath=path.join(__dirname,'../templates/partials')
+
+
+
+app.set('view engine','hbs')
+app.set('views',viewsPath)
+hbs.registerPartials(partialPath);
+
+
+app.use(express.static(publicDirPath))
+app.get('',(req,res)=>{
+    res.render('index',{
+        title:'weather',
+        name:'nuruddin'
+    })
+})
+app.get('/about',(req,res)=>{
+    res.render('about',{
+        title:'about',
+        name:'nuruddin'
+    })
+})
+app.get('/help',(req,res)=>{
+    res.render('help',{
+        title:'help ',
+        name:'nuruddin'
+    })
+})
+
+app.get('/weather',(req,res)=>{
+
+    if(!req.query.address){
+        return res.send({
+            error:'please provide an address'
+        })
+    }
+    geoCode(req.query.address,(error,{latitude,longtitude,location}={})=>{
+        if(error){
+            return res.send({
+                error:error
+            })
+        }
+    
+        forecast(latitude, longtitude, (error, forecastData) => {
+            if(error){
+                return res.send({
+                    error:error
+                })
+            }
+            res.send({
+                location:location,
+                forecast:forecastData.temp,
+                address:req.query.address
+            })
+            
+        })
+    })
+    
+})
+
+app.get('/help/*',(req,res)=>{
+    res.render('error',{
+        title:'help ',
+        name:'nuruddin',
+        msg:'Help article not found'
+    })
+})
+
+app.get('*',(req,res)=>{
+    res.render('error',{
+  
+        name:'nuruddin',
+        msg:'Page not found'
+    })
+})
+
+app.listen(3000,()=>{
+    console.log('server running on port 30000')
+})
---
+++ src/utils/forecast.js
@@ -0,0 +1,22 @@
+const request = require("request")
+
+
+const forecast=(latitude,longtitude,callback)=>{
+    const url='http://api.weatherstack.com/current?access_key=daf08710d5ca1938af22d31209ccc38f&query='+latitude+','+longtitude+'&units=m'
+    request({url,json : true },(error,{body})=>{ 
+        if(error){
+            callback('unable to connect to the weather server',undefined)
+        }else if(body.error){
+            callback('no result found',undefined)
+        }else{
+      
+            callback(undefined,{
+                temp:body.current.temperature,
+                feel:body.current.feelslike
+            })
+            
+        }
+    })
+}
+
+module.exports=forecast
